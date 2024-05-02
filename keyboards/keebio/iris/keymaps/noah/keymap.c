4531a90d99fb832b15eba2a643ce09e0ea105533
Noah Johnson <njohnson@omnitech-inc.com>
2024-05-02 15:34:28 -0600

    lol

---
+++ keyboards/keebio/iris/keymaps/noah/keymap.c
@@ -0,0 +1,160 @@
+#include QMK_KEYBOARD_H
+
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 3
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+  WIN_UP,
+  WIN_DN
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LALT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_DEL,          KC_RGUI,  KC_N,    KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_EQL,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    KC_BSPC, KC_LCTL, KC_LSFT,                   KC_SPC,   MO(1),   KC_ENT
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_LOWER] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_ESC,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,                             KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10, KC_SLEP,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB,  KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR,  KC_PERC,                           KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS,  KC_PMNS, KC_PLUS,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_DEL,  KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_LBRC,                            KC_RBRC,  KC_LEFT,  KC_UP, KC_DOWN, KC_RGHT,  WIN_UP,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LALT, _______, _______,  _______, KC_PSTE, KC_F11,  KC_ENT,           KC_RGUI, KC_F12,  KC_HOME, KC_PGUP, KC_PGDN, KC_END,  WIN_DN,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    KC_BSPC, KC_LCTL, KC_LSFT,                    KC_SPC,  MO(1),  TO(2)
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_RAISE] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, RGB_SPD, RGB_SPI, _______, _______, _______,                            _______, KC_BRIU, KC_BRID, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     RGB_TOG, RGB_VAD, RGB_VAI, RGB_RMOD, RGB_MOD, KC_UNDS,                           _______, KC_VOLU, KC_VOLD, KC_MPLY, KC_MRWD, KC_MFFD,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, RGB_SAD, RGB_SAI, RGB_HUI, RGB_HUD, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                   _______, TO(0),    TO(1)
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_ADJUST] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                   _______, _______, _______
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+    case WIN_UP:
+    if (record->event.pressed) {
+      register_code(KC_LWIN);
+      register_code(KC_LSFT);
+      register_code(KC_LEFT);
+      unregister_code(KC_LEFT);
+      unregister_code(KC_LSFT);
+      unregister_code(KC_LWIN);
+    }
+    case WIN_DN:
+    if (record->event.pressed) {
+      register_code(KC_LWIN);
+      register_code(KC_LSFT);
+      register_code(KC_RGHT);
+      unregister_code(KC_RGHT);
+      unregister_code(KC_LSFT);
+      unregister_code(KC_LWIN);
+    }
+    return false;
+    break;
+  }
+  return true;
+}
+
+
+/*
+
+// Uncomment for layer-specific LEDs
+
+#define RBG_VAL 100
+layer_state_t layer_state_set_user(layer_state_t state) {
+  switch(biton32(state)) {
+  case _LOWER:
+    // green-ish
+    rgblight_sethsv_noeeprom(1, 221, RBG_VAL);
+    break;
+  case _RAISE:
+    // Dark Blue
+    rgblight_sethsv_noeeprom(180, 221, RBG_VAL);
+    break;
+  default:
+    // Default colors
+    rgblight_sethsv_noeeprom(370, 221, RBG_VAL);
+    break;
+  }
+  return state;
+}
+
+*/
